569433fa8421dd0b96a966f25503a4f311b92e2d
kaspar moberg <kaspeti123@gmail.com>
2024-03-18 13:32:22 +0100

    added map topology json and serve from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, send_file
 from data import get_location_data
 
 
@@ -10,11 +10,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/api/map")
-def get_map():
+@app.route("/api/locations")
+def locations():
     map_data = get_location_data()
 
     return Response(
             map_data.to_json(orient="records"),
             mimetype="application/json"
     )
+
+
+@app.route("/api/map-topology")
+def map_topology():
+    return send_file("./static/data/countries-110m.json", mimetype="application/json")
