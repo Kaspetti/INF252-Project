6e1067e0e0f20b11fa89d0e5033d8966608f7281
Kaspar Moberg <kaspeti123@gmail.com>
2024-04-01 16:51:05 +0200

    added drawing of map and centroids

--- data.py
+++ data.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 def get_location_data():
     df = pd.read_csv("./static/data/data.csv")
-    return df[["Centroid.Longitude", "Centroid.Latitude"]]
+    df = df.rename(columns={"Centroid.Longitude": "Longitude", "Centroid.Latitude": "Latitude"})
+    return df[["Longitude", "Latitude"]]
---
+++ static/js/geomap.js
@@ -0,0 +1,47 @@
+/// <reference types="d3" />
+/// <reference types="topojson" />
+
+async function showMap() {
+  let topology = await d3.json("/api/map-topology")
+  console.log(topology)
+
+  let features = topojson.feature(topology, topology.objects.countries).features;
+  console.log(features)
+
+  let data = await d3.json("api/locations")
+  console.log(data[0])
+
+  const xAccessor = d => path.projection()([d.Longitude, d.Latitude])[0]
+  const yAccessor = d => path.projection()([d.Longitude, d.Latitude])[1]
+
+  const path = d3.geoPath().projection(d3.geoEqualEarth())
+
+  const svg = d3.select("#map")
+    .append("svg")
+    .attr("width", 975)
+    .attr("height", 610)
+    .attr("viewBox", [0, 0, 975, 610])
+    .attr("style", "width: 100%; height: auto; height: intrinsic;");
+
+
+  // Draw the map
+  svg.append("path")
+    .datum(topojson.feature(topology, topology.objects.countries))
+    .attr("fill", "#fff")
+    .attr("stroke", "black")
+    .attr("d", path)  
+
+  
+  // Draw all centroids on the map
+  svg.append("g")
+    .attr("fill", "red")
+    .attr("fill-opacity", 0.25)
+    .selectAll()
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xAccessor(d))
+    .attr("cy", d => yAccessor(d))
+    .attr("r", 1)
+}
+
+showMap();
